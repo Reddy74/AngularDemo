eccc7bbea1b8bfa347b5e93775e9d6278da696a1
Reddy74 <39188964+Reddy74@users.noreply.github.com>
2023-03-17 12:21:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setups
+// view engine setupss
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
